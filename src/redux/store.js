2738fee027ed4bdcf7eeb400fb2af4e09022aeae
smit <smitjiwani44@gmail.com>
2023-09-15 17:18:18 +0530

    commit 4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./todoSlice";
+import todoReducer from "./TodoSlice";
 
 export const store = configureStore({
     reducer: todoReducer,
